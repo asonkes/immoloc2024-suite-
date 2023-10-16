fc66ecbe7c91736bc8799aeb22efe1190507ccba
asonkes <audrey.sonkes@gmail.com>
2023-10-16 12:04:59 +0200

    first commit

---
+++ src/Controller/AdController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Image;
+use App\Form\AnnonceType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+
+    /**
+     * Permet d'afficher l'ensemble des annonces du site
+     *
+     * @param AdRepository $repo
+     * @return Response
+     */
+    #[Route('/ads', name: 'ads_index')]
+    public function index(AdRepository $repo): Response
+    {
+
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+
+    /**
+     * Permet d'ajouter une annonce à la bdd 
+     *
+     * @return Response
+     */
+    #[Route("/ads/new", name: "ads_create")]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+
+        $ad = new Ad();
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        // $arrayForm = $request->request->all();
+        // dump($arrayForm);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // gestion des images 
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+
+            // je persiste mon objet Ad
+            $manager->persist($ad);
+            // j'envoie les persistances dans la bdd
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>" . $ad->getTitle() . "</strong> a bien été enregistrée"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render("ad/new.html.twig", [
+            'myForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'éditier une annonce
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param Ad $ad
+     * @return Response
+     */
+    #[Route("/ads/{slug}/edit", name: "ads_edit")]
+    public function edit(Request $request, EntityManagerInterface $manager, Ad $ad): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // gestion des images
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>" . $ad->getTitle() . "</strong> a bien été modifiée!"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render("ad/edit.html.twig", [
+            "ad" => $ad,
+            "myForm" => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une annonce 
+     * 
+     * @param string $slug
+     * @param Ad $ad
+     * @return Response
+     */
+    #[Route("/ads/{slug}", name: "ads_show")]
+    public function show(string $slug, Ad $ad): Response
+    {
+        // $ad = $repo->findby(["slug"=>$slug])
+
+        // dump($ad);
+
+        return $this->render("ad/show.html.twig", [
+            'ad' => $ad
+        ]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Image;
+use Cocur\Slugify\Slugify;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $slugify = new Slugify();
+
+
+        // gestion des utilisateurs 
+        $users = []; // init d'un tableau pour rÃ©cup des user pour les annonces
+        $genres = ['male', 'femelle'];
+
+        for ($u = 1; $u <= 10; $u++) {
+            $user = new User();
+            $genre = $faker->randomElement($genres);
+            $picture = 'https://picsum.photos/seed/picsum/500/500';
+
+            $hash = $this->passwordHasher->hashPassword($user, 'password');
+
+            $user->setFirstName($faker->firstName($genre))
+                ->setLastName($faker->lastName())
+                ->setEmail($faker->email())
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                ->setPassword($hash)
+                ->setPicture($picture);
+
+            $manager->persist($user);
+
+            $users[] = $user; // ajouter un user au tableau (pour les annonces)
+
+        }
+
+
+
+        for ($i = 1; $i <= 30; $i++) {
+            $ad = new Ad();
+            $title = $faker->sentence();
+            $coverImage = 'https://picsum.photos/seed/picsum/1000/350';
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+            // liaison avec l'user
+            $user = $users[rand(0, count($users) - 1)];
+
+            $ad->setTitle($title)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(rand(40, 200))
+                ->setRooms(rand(1, 5))
+                ->setAuthor($user);
+
+
+            // Gestion de la galerie image de l'annonce
+            for ($g = 1; $g <= rand(2, 5); $g++) {
+                $image = new Image();
+                $image->setUrl('https://picsum.photos/id/' . $g . '/900')
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+        }
+
+        $manager->flush();
+    }
+}
